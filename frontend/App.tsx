e6871e7ae3db43fe565dfbcc69cb186cbcd55487
Richard Guach <guach288@gmail.com>
2023-07-18 12:43:32 -0500

    implemented a callback in card buttons

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -9,8 +9,16 @@ export default function App() {
     <>
 
       <View style={styles.Cards}>
-        <ProductCard image='https://images.pexels.com/photos/45982/pexels-photo-45982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' name='Saco' description='Saco de lana para el invierno' price={6}></ProductCard>
-        <ProductCard image='https://images.pexels.com/photos/1082528/pexels-photo-1082528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' name='Pantalon' description='Pantalón jean' price={20}></ProductCard>
+        <ProductCard image='https://images.pexels.com/photos/45982/pexels-photo-45982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+        name='Saco' 
+        description='Saco de lana para el invierno' 
+        price={6} ></ProductCard>
+
+        <ProductCard image='https://images.pexels.com/photos/1082528/pexels-photo-1082528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+        name='Pantalon' 
+        description='Pantalón jean' 
+        price={20} 
+        callback={()=>{alert("COMPRA")}}></ProductCard>
       </View>
     </>
   );
--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "../types/Product";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import { View, Text, Image, StyleSheet, Button, Alert } from "react-native";
 import CustomButton from "./CustomButton";
 
 const styles = StyleSheet.create({
@@ -34,19 +34,28 @@ const styles = StyleSheet.create({
     width:"fit-content",
     height:35,
     padding:10,
-    borderRadius:15
+    borderRadius:15,
+    borderStyle:"solid",
+    borderWidth:1,
   }
 
 });
 
-export const ProductCard:React.FC<Product>  = (props)=> {
-  let {image, name, description, price, } =  props;
+interface CallbackFunction{
+  ():void
+}
+
+export const ProductCard:React.FC<Product & {buttonColor?:string, callback?:CallbackFunction}>  = (props)=> {
+  let {image, name, description, price, buttonColor: color, callback} =  props;
+  const defaultOnclick = () => alert(`usted ha comprado ${name} con el precio de $${price}USD`);
+
   return <View style={styles.container}>
     <Image source={{uri:image}} alt={name} style={styles.Image}/>
     <Text style={styles.TitleText}>{name}</Text>
     <Text style={styles.Text}>{description}</Text>
     <Text style={styles.Text}>$ {price} USD</Text>
-    <CustomButton title="Comprar" color="#00ADB5" onPress={()=>{}}></CustomButton>
+
+    <CustomButton title="Comprar" color={color ?? "#00ADB5"} onPress={callback ?? defaultOnclick}></CustomButton>
   </View>
 }
 
